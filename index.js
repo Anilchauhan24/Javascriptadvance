e9e63e287f485e0daf925895a3887c6b445f2450
Anilchauhan10 <bankingidealoan@gmail.com>
2023-11-20 21:27:05 +0530

    updata

---
+++ index.js
@@ -0,0 +1,65 @@
+// function value(nul) {
+//     nul.name = "Anil chauhan";
+// }
+
+
+// let person = new Object();
+// value(person)
+// console.log(person.name)
+
+
+
+
+
+// function setName(obj) {
+//     obj.name = "Anil chauhan";
+// }
+
+// let person = new Object();
+// setName(person);
+// console.log(person.name);
+
+
+// function addFive(value) {
+//     value += 100;
+//     return value;
+// }
+
+// let count = 200;
+// let newValue = addFive(count);
+// console.log(newValue)
+
+// function addTen(num) {
+//     num += 10;
+//     return num;
+
+// }
+// let count = 20;
+// let result = addTen(count);
+// console.log(result)
+
+// // let num1 = 'Anil chauhan';
+// // let num2 = num1;
+// // console.log(num2
+
+
+// // let person = ['Home', 'Class', 'section', 'about us'];
+
+// // // person.forEach(item => {
+// // //     let yes = document.getElementById('yes');
+// // //     yes.innerHTML += item + '<br>'; // You can use innerHTML to append content
+// // // });
+
+// // person.forEach(value);
+
+// // function value(item) {
+// //     let yes = document.getElementById("yes");
+// //     yes.innerHTML += item + '<br>';
+// // }
+
+// // person.forEach(value);
+
+// // function value(index) {
+// //     let dot = document.getElementById("yes");
+// //     dot.innerHTML += index + "<br>";
+// // }
