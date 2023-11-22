43904549a6111e7e30182eb9c6681c0f694ff2b8
Anilchauhan10 <bankingidealoan@gmail.com>
2023-11-22 11:42:13 +0530

    update

--- index.js
+++ index.js
@@ -1,3 +1,145 @@
+
+
+// let element = document.getElementById("some_element");
+// let myObject = new Object();
+// myObject.element = element;
+// element.someOtherObject = myObject;
+// function problem(){
+//     let objectA= new Object();
+//     let objectB =new Object();
+
+//     objectA.someOtherObject= objectB;
+// }
+
+// const ol = {};
+// ol = {};
+
+// const o2 = {};
+// o2.name = "jake";
+// console.log(o2.name);
+
+// const o3 = Object.freeze({});
+// o3.name = "jake";
+// console.log(o3.name);
+
+
+
+
+// var y = 0;
+// for (let i = 0; i < 10; i++) {
+//     if (i % 3 === 0) {
+//         continue;
+//     }
+//     y++;
+//     console.log(y)
+// }
+// console.log(y)
+// 1- 1
+// 3- 2
+// 4-
+// 5- 3
+// 6
+// 7-4
+// 9- 5
+// 10-
+
+// output are- 1,2,3,4,5,5
+
+
+// 0-
+// 1- 1
+// 2- 2
+// 3-
+// 4-3
+// 5-4
+// 6-
+// 7-5
+// 8-6
+// 9-
+
+// output are 1,2,3,4,5,6,6
+
+// if (true) {
+//     let a;
+
+// }
+
+// console.log(a);
+
+// while (true) {
+//     let b;
+// }
+// console.log(b);
+
+// function foo() {
+//     let c;
+
+// }
+// console.log(c);
+
+// function value(num1, num2) {
+//     sum = num1 + num2;
+
+
+// }
+// var r = value(12, 12);
+// console.log(r); //undefined
+
+
+// function add(num1, num2) {
+//     var sum = num1 + num2;
+//     console.log(sum)
+// }
+
+// var d = add(40, 30)
+// console.log(d)
+
+// var color = "blue";
+// function changeColor() {
+//     let anotherColor = "red";
+//     function swapColor() {
+//         let tempColor = anotherColor;
+//         anotherColor = color;
+//         color = tempColor;
+//         console.log(color)
+//     }
+//     swapColor()
+//     console.log(color)
+// }
+// console.log(color)
+
+// changeColor()
+
+
+
+
+// var color = "blue";
+
+// function changeColor() {
+//     if (color === "blue") {
+//         color = "red";
+//     }
+//     else {
+//         color = 'blue';
+//     }
+// }
+
+// let glue = changeColor();
+// console.log(glue)// undefined
+
+// let s = "anil";
+// let p = true;
+// let i = 23;
+// let u;
+// let n = new Object();
+
+// console.log(typeof (s))//string
+// console.log(typeof (p)) //boolean
+// console.log(typeof (i)) //numer
+// console.log(typeof (u))//unde
+// console.log(typeof (n));//objec
+
+
 // function value(nul) {
 //     nul.name = "Anil chauhan";
 // }
