43b034eac0ce4dc82eff0f36e72ac53c48cab351
Anilchauhan10 <bankingidealoan@gmail.com>
2023-11-23 22:59:02 +0530

    update

---
+++ part3.js
@@ -0,0 +1,33 @@
+let stringValue = "Chauhan";
+let newValue = stringValue.padStart(17, "Anil Kumar");
+let ul = document.getElementById("ul");
+let li = document.createElement("li");
+ul.textContent = newValue;
+ul.appendChild("li");
+
+
+// let stringValue = "na";
+// console.log(stringValue.repeat(5));
+
+
+
+
+
+//trim method help to get the copy of the strings;
+
+// let stringValue = "Hello world";
+
+// let trimedStringValue = stringValue.trim();
+
+// console.log(trimedStringValue);//Hello world
+
+// let value = "Anil";
+// document.write(value.trim());
+
+// let hello = "Anilchauhan";
+// let bolo = hello.endsWith("han", 11);
+// console.log(bolo);// true;
+// console.log(hello.endsWith("n"), 13);//true
+// let message = "footbal2";
+// console.log(message.endsWith("bal2"));
+// //endsWith - check the lat value and typ
