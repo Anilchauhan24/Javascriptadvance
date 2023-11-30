ebb3096e6fa8b43cb993c73decf12e6a5828e390
Anilchauhan10 <bankingidealoan@gmail.com>
2023-11-30 15:00:14 +0530

    update

---
+++ part6.js
@@ -0,0 +1,23 @@
+// Solve the following equation:
+
+// (2p + 3) – 4 = p
+
+
+function solution() {
+    let p = 10;
+    let eqnOne = p;
+    let z = 4;
+    let eqnTwo = (2 * (eqnOne) + 3) - z;
+
+    console.log(eqnTwo)//19
+
+}
+solution()
+
+
+const p = 10;
+const eqnOne = p;
+const z = 4;
+const eqnTwo = (2 * (eqnOne) + 3) - z;
+
+document.write(eqnTwo)
